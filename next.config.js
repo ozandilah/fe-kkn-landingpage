2f7c5d4334d2137e1d3d704e39460f8359c9a278
ozandilah <79375569+ozandilah@users.noreply.github.com>
2023-07-26 15:09:21 +0700

    Setup Pertama

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
-}
+  experimental: {
+    newNextLinkBehavior: false,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg%/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("./node_modules/designsystem/colors");
+module.exports = {
+  content: [
+    "./src/**/*.{js,tx,jsx,tsx}",
+    "node_modules/designsystem/dist/**/*.{js,tx,jsx,tsx}",
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        ...colors,
+      },
+      ring: {
+        ...colors,
+      },
+      fill: {
+        ...colors,
+        "yellow-star": "#F3A939",
+      },
+      fontFamily: {
+        sans: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
